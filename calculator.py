c4d4ade47a0581f73ea48ee696dc33fa229b0968
BuckyRogers08 <cachikhale@gmail.com>
2025-05-28 21:00:19 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,23 +1,24 @@
-A =float(input("A = "))
-B =float(input("B = "))
+#input:
+A =float(input("Enter value for A:"))
+B =float(input("Enter value for B:"))
 
-ans= input("enter operation(+,-,*,%,/):")
+#operations input:
+ans= input("Enter operation(+,-,*,%,/,//,**):")
 
+#logic, loops and output statements:
 if ans=='+':
   print("ANS =",A+B)
 elif ans=='-':
   print("ANS =",A-B)
 elif ans=='*':
   print("ANS =",A*B)
-elif ans=='%':
-  if B==0:
-   print("error")
-  else:
-     print("the remainder is",A%B)
 elif ans=='/':
-  if B==0:
-    print("error")
-  else:
-    print("ANS =",A/B)
+  print("ANS =" if B != 0 else "Error", A / B if B != 0 else "")
+elif ans=='%':
+  print("The remainder is" if B!= 0 else "Error: Modulo by 0",A%B if B!=0 else"")
+elif ans=='//':
+   print("ANS =" if B != 0 else "Error: Floor division by zero", A // B if B != 0 else "")
+elif ans=='**':
+  print("ANS =",A**B)
 else:
   print("invalid operation")
